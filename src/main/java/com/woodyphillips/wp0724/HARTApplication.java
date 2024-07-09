6949b327b4b8d61f9139af7b99ac5e0c327b8430
MicahWoodrowPhillips <micah.w.phillips@gmail.com>
2024-07-09 13:13:36 -0400

    Finally back to this.  Added empty tests, a basic test as a template, basic kind of database stand-in HardwareTool class (empty) and a Main class to run this in the CLI.  Will add a little more to it so it can scan for commands in the terminal next so that I have an easier time playing around with it, then get going on the tests.  Will also make a roadmap in git with issues.

--- src/main/java/com/woodyphillips/wp0724/HARTApplication.java
+++ src/main/java/com/woodyphillips/wp0724/HARTApplication.java
@@ -18,7 +18,7 @@ public class HARTApplication extends Thread {
     }
 
     public boolean receivedInput(String s) {
-        if (s != null && s.length() > 0) {
+        if (s != null && s.isEmpty()) {
             return true;
         }
         return false;
